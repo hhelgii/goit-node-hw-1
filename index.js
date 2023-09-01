b321dff6eb249da69f69d14a4b3de0f1157906bb
hhelgii <opruska2004@gmail.com>
2023-09-01 10:11:28 +0300

    deleted comments

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
 import contactsService from "./contacts.js";
-// import yargs from "yargs";
-// const invokeAction = async ({ action, id, email, name, phone }) => {
-//   switch (action) {
-//     case "list":
-//       const allContacts = await contactsService.listContacts();
-//       return console.log(allContacts);
-//     case "get":
-//       const contact = await contactsService.getContactById(id);
-//       return console.log(contact);
-//     case "add":
-//       const newContact = await contactsService.addContact(name, email,phone);
-//       return console.log(newContact);
-//     case "remove":
-//       const removeContact = await contactsService.removeContact(id);
-//       return console.log(removeContact);
-//     default:
-//       console.log("Unknown Action");
-//   }
-// };
-
-// const {argv} = yargs(process.argv.slice(2));
-// invokeAction(argv);
 import { program } from "commander";
 
 program
